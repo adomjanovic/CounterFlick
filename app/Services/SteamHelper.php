cef7a8aa842200812dc94af8a2fbf227928a4052
AnteD <antedomio2.net.hr>
2017-09-18 13:07:15 +0200

    version 1.0

--- app/Services/SteamHelper.php
+++ app/Services/SteamHelper.php
@@ -4,8 +4,7 @@ namespace App\Services;
 
 class SteamHelper
 {
-    const API_KEY = '23E38BACEF40A739B05B305A8487184C';
-    //const API_KEY = 'PUT_YOUR_API_KEY_FROM_STEAM';
+    const API_KEY = 'PUT_YOUR_API_KEY_FROM_STEAM';
     public static function findSteamPlayerUri()
     {
         $uri = explode('/', $_SERVER['REQUEST_URI']);
